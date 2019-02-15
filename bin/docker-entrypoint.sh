c3c24546619a260a5afd6344098cf90bbee2727f
em <eugen.mayer@kontextwork.de>
2019-02-15 09:37:48 +0100

    Add support for gradle in commands

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -6,7 +6,8 @@
 if [ "$1" = "java" ]; then
     # the case where the user uses his own java -jar startup command - let him do that
     exec "$@" > >(tee -a ${LOG_BASE_DIR}/app.log) 2> >(tee -a ${LOG_BASE_DIR}/app.err >&2)
+elif [ "$1" = "./gradlew" ]; then
+    exec "$@"
 else
     exec java -jar ${JAR_FILE_BASEDIR}/${JAR_FILE_NAME} "$@" > >(tee -a ${LOG_BASE_DIR}/app.log) 2> >(tee -a ${LOG_BASE_DIR}/app.err >&2)
 fi
-
